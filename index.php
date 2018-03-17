5b6f208b38821483368795ca2fe160b849c047de
Eve Lin <eve.d.711@gmail.com>
2018-03-17 16:29:09 +0100

    First Commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<p>
+  <?php
+echo "<h1>Hello World\n</h1>";
+$answer=6*7;
+echo "The answer is $answer, what was the question again?\n";
+ ?>
+</p>
+<p>Such a beautiful day</p>
+<?php
+$stuff=array("name"=>"Eve",
+"Surname"=>"Lin");
+echo("<pre>\n");
+print_r($stuff);
+echo("\n</pre\n");
+?>
